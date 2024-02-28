2fd2a3ad7f21f6c928e1797199834aa7c79f4eb4
Dumbledore-BLR🤪 <150517265+dumbledore-blr@users.noreply.github.com>
2024-02-28 11:45:09 +0530

    Update main_script.py

--- main_script.py
+++ main_script.py
@@ -63,7 +63,7 @@ st.set_page_config(page_title="Extraction Application")
 # Custom CSS style
 cus_css = """
 body {
-background-color: #ffffff;
+background-color: #333333;
 
 opacity: 0.9;
 background-image:  linear-gradient(30deg, #000000 12%, transparent 12.5%, transparent 87%, #000000 87.5%, #000000), linear-gradient(150deg, #000000 12%, transparent 12.5%, transparent 87%, #000000 87.5%, #000000), linear-gradient(30deg, #000000 12%, transparent 12.5%, transparent 87%, #000000 87.5%, #000000), linear-gradient(150deg, #000000 12%, transparent 12.5%, transparent 87%, #000000 87.5%, #000000), linear-gradient(60deg, #00000077 25%, transparent 25.5%, transparent 75%, #00000077 75%, #00000077), linear-gradient(60deg, #00000077 25%, transparent 25.5%, transparent 75%, #00000077 75%, #00000077);
